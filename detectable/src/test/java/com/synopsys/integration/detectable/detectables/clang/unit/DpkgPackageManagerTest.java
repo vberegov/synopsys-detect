362a911f3f91258b82290729a423ee5bb8d7bd09
Steve Billings <sbillings@blackducksoftware.com>
2020-06-29 16:19:06 -0400

    test: refactoring tests

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/clang/unit/DpkgPackageManagerTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/clang/unit/DpkgPackageManagerTest.java
@@ -27,6 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Arrays;
 import java.util.List;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -41,49 +42,17 @@ import com.synopsys.integration.detectable.detectables.clang.packagemanager.reso
 
 public class DpkgPackageManagerTest {
 
-    //TODO: Split into 2 tests - one for VersionResolution and one for FullResolution
-
     @Test
     public void test() throws ExecutableRunnerException, NotOwnedByAnyPkgException {
-        StringBuilder sb = new StringBuilder();
-        sb.append("garbage\n");
-        sb.append("nonsense\n");
-        sb.append("libc6-dev:amd64: /usr/include/stdlib.h\n");
-        final String pkgMgrOwnedByOutput = sb.toString();
+        String pkgMgrOwnedByOutput = getDpkgOwnedByOutputLibC6Dev();
+        String pkgMgrVersionOutput = getDpkgVersionOutputLibC6Dev();
+        ExecutableRunner executableRunner = Mockito.mock(ExecutableRunner.class);
+        Mockito.when(executableRunner.execute(null, "dpkg", Arrays.asList("-s", "libc6-dev"))).thenReturn(new ExecutableOutput("", 0, pkgMgrVersionOutput, ""));
 
-        sb = new StringBuilder();
-        sb.append("Package: libc6-dev\n");
-        sb.append("Status: install ok installed\n");
-        sb.append("Priority: optional\n");
-        sb.append("Section: libdevel\n");
-        sb.append("Installed-Size: 18812\n");
-        sb.append("Maintainer: Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>\n");
-        sb.append("Architecture: amd64\n");
-        sb.append("Multi-Arch: same\n");
-        sb.append("Source: glibc\n");
-        sb.append("Version: 2.27-3ubuntu1\n");
-        sb.append("Provides: libc-dev\n");
-        sb.append("Depends: libc6 (= 2.27-3ubuntu1), libc-dev-bin (= 2.27-3ubuntu1), linux-libc-dev\n");
-        sb.append("Suggests: glibc-doc, manpages-dev\n");
-        sb.append(
-            "Breaks: binutils (<< 2.26), binutils-gold (<< 2.20.1-11), cmake (<< 2.8.4+dfsg.1-5), gcc-4.4 (<< 4.4.6-4), gcc-4.5 (<< 4.5.3-2), gcc-4.6 (<< 4.6.0-12), libhwloc-dev (<< 1.2-3), libjna-java (<< 3.2.7-4), liblouis-dev (<< 2.3.0-2), liblouisxml-dev (<< 2.4.0-2), libperl5.26 (<< 5.26.1-3), make (<< 3.81-8.1), pkg-config (<< 0.26-1)\n");
-        sb.append("Conflicts: libc0.1-dev, libc0.3-dev, libc6.1-dev\n");
-        sb.append("Description: GNU C Library: Development Libraries and Header Files\n");
-        sb.append(" Contains the symlinks, headers, and object files needed to compile\n");
-        sb.append(" and link programs which use the standard C library.\n");
-        sb.append("Homepage: https://www.gnu.org/software/libc/libc.html\n");
-        sb.append("Original-Maintainer: GNU Libc Maintainers <debian-glibc@lists.debian.org>\n");
+        DpkgVersionResolver dpkgVersionResolver = new DpkgVersionResolver();
+        DpkgPackageManagerResolver pkgMgr = new DpkgPackageManagerResolver(dpkgVersionResolver);
 
-        final String pkgMgrVersionOutput = sb.toString();
-
-        final String packageName = "libc6-dev";
-        final ExecutableRunner executableRunner = Mockito.mock(ExecutableRunner.class);
-        Mockito.when(executableRunner.execute(null, "dpkg", Arrays.asList("-s", packageName))).thenReturn(new ExecutableOutput("", 0, pkgMgrVersionOutput, ""));
-
-        final DpkgVersionResolver dpkgVersionResolver = new DpkgVersionResolver();
-        final DpkgPackageManagerResolver pkgMgr = new DpkgPackageManagerResolver(dpkgVersionResolver);
-
-        final List<PackageDetails> pkgs = pkgMgr.resolvePackages(new ClangPackageManagerInfoFactory().dpkg(), executableRunner, null, pkgMgrOwnedByOutput);
+        List<PackageDetails> pkgs = pkgMgr.resolvePackages(new ClangPackageManagerInfoFactory().dpkg(), executableRunner, null, pkgMgrOwnedByOutput);
 
         assertEquals(1, pkgs.size());
         assertEquals("libc6-dev", pkgs.get(0).getPackageName());
@@ -94,14 +63,27 @@ public class DpkgPackageManagerTest {
     @Test
     public void testEpoch() throws ExecutableRunnerException, NotOwnedByAnyPkgException {
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("garbage\n");
-        sb.append("nonsense\n");
-        sb.append("login:amd64: /usr/include/stdlib.h\n");
-        final String pkgMgrOwnedByOutput = sb.toString();
+        String pkgMgrOwnedByOutput = getDpkgOwnedByOutputStdLib();
+        String pkgMgrVersionOutput = getDpkgVersionOutputStdLib();
 
+        ExecutableRunner executableRunner = Mockito.mock(ExecutableRunner.class);
+        Mockito.when(executableRunner.execute(null, "dpkg", Arrays.asList("-s", "login"))).thenReturn(new ExecutableOutput("", 0, pkgMgrVersionOutput, ""));
+
+        DpkgVersionResolver dpkgVersionResolver = new DpkgVersionResolver();
+        DpkgPackageManagerResolver pkgMgr = new DpkgPackageManagerResolver(dpkgVersionResolver);
+
+        List<PackageDetails> pkgs = pkgMgr.resolvePackages(new ClangPackageManagerInfoFactory().dpkg(), executableRunner, null, pkgMgrOwnedByOutput);
+
+        assertEquals(1, pkgs.size());
+        assertEquals("login", pkgs.get(0).getPackageName());
+        assertEquals("1:4.5-1ubuntu1", pkgs.get(0).getPackageVersion());
+        assertEquals("amd64", pkgs.get(0).getPackageArch());
+    }
+
+    @NotNull
+    private String getDpkgVersionOutputStdLib() {
+        StringBuilder sb;
         sb = new StringBuilder();
-
         sb.append("Package: login\n");
         sb.append("Essential: yes\n");
         sb.append("Status: install ok installed\n");
@@ -128,22 +110,52 @@ public class DpkgPackageManagerTest {
         sb.append(" user ID (useful being able to execute commands as another user).\n");
         sb.append("Homepage: https://github.com/shadow-maint/shadow\n");
         sb.append("Original-Maintainer: Shadow package maintainers <pkg-shadow-devel@lists.alioth.debian.org>\n");
-
-        final String pkgMgrVersionOutput = sb.toString();
-
-        final String packageName = "login";
-        final ExecutableRunner executableRunner = Mockito.mock(ExecutableRunner.class);
-        Mockito.when(executableRunner.execute(null, "dpkg", Arrays.asList("-s", packageName))).thenReturn(new ExecutableOutput("", 0, pkgMgrVersionOutput, ""));
-
-        final DpkgVersionResolver dpkgVersionResolver = new DpkgVersionResolver();
-        final DpkgPackageManagerResolver pkgMgr = new DpkgPackageManagerResolver(dpkgVersionResolver);
-
-        final List<PackageDetails> pkgs = pkgMgr.resolvePackages(new ClangPackageManagerInfoFactory().dpkg(), executableRunner, null, pkgMgrOwnedByOutput);
-
-        assertEquals(1, pkgs.size());
-        assertEquals("login", pkgs.get(0).getPackageName());
-        assertEquals("1:4.5-1ubuntu1", pkgs.get(0).getPackageVersion());
-        assertEquals("amd64", pkgs.get(0).getPackageArch());
+        return sb.toString();
     }
 
+    @NotNull
+    private String getDpkgOwnedByOutputStdLib() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("garbage\n");
+        sb.append("nonsense\n");
+        sb.append("login:amd64: /usr/include/stdlib.h\n");
+        return sb.toString();
+    }
+
+    @NotNull
+    private String getDpkgOwnedByOutputLibC6Dev() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("garbage\n");
+        sb.append("nonsense\n");
+        sb.append("libc6-dev:amd64: /usr/include/stdlib.h\n");
+        return sb.toString();
+    }
+
+    @NotNull
+    private String getDpkgVersionOutputLibC6Dev() {
+        StringBuilder sb;
+        sb = new StringBuilder();
+        sb.append("Package: libc6-dev\n");
+        sb.append("Status: install ok installed\n");
+        sb.append("Priority: optional\n");
+        sb.append("Section: libdevel\n");
+        sb.append("Installed-Size: 18812\n");
+        sb.append("Maintainer: Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>\n");
+        sb.append("Architecture: amd64\n");
+        sb.append("Multi-Arch: same\n");
+        sb.append("Source: glibc\n");
+        sb.append("Version: 2.27-3ubuntu1\n");
+        sb.append("Provides: libc-dev\n");
+        sb.append("Depends: libc6 (= 2.27-3ubuntu1), libc-dev-bin (= 2.27-3ubuntu1), linux-libc-dev\n");
+        sb.append("Suggests: glibc-doc, manpages-dev\n");
+        sb.append(
+            "Breaks: binutils (<< 2.26), binutils-gold (<< 2.20.1-11), cmake (<< 2.8.4+dfsg.1-5), gcc-4.4 (<< 4.4.6-4), gcc-4.5 (<< 4.5.3-2), gcc-4.6 (<< 4.6.0-12), libhwloc-dev (<< 1.2-3), libjna-java (<< 3.2.7-4), liblouis-dev (<< 2.3.0-2), liblouisxml-dev (<< 2.4.0-2), libperl5.26 (<< 5.26.1-3), make (<< 3.81-8.1), pkg-config (<< 0.26-1)\n");
+        sb.append("Conflicts: libc0.1-dev, libc0.3-dev, libc6.1-dev\n");
+        sb.append("Description: GNU C Library: Development Libraries and Header Files\n");
+        sb.append(" Contains the symlinks, headers, and object files needed to compile\n");
+        sb.append(" and link programs which use the standard C library.\n");
+        sb.append("Homepage: https://www.gnu.org/software/libc/libc.html\n");
+        sb.append("Original-Maintainer: GNU Libc Maintainers <debian-glibc@lists.debian.org>\n");
+        return sb.toString();
+    }
 }
